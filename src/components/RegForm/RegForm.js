695c9381c3b437d012728a85231e046cfb9884dd
Egor Dorosh <egor.dorosh@libertexgroup.com>
2021-05-15 09:04:41 +0200

    Refactoring reg form component

--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,13 +1,14 @@
-import React, { useReducer, useRef, useState } from 'react';
+import React, { useReducer, useRef, useState } from "react";
 
-import Card from '../UI/Card/Card';
-import Input from '../UI/Input/Input';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
-import classes from './RegForm.module.scss';
+import classes from "./RegForm.module.scss";
 
 function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
@@ -16,38 +17,38 @@ function validatePassword(pass) {
 }
 
 function fieldReducer(state, action, validator) {
-  if (action.type === 'USER_INPUT') {
+  if (action.type === "USER_INPUT") {
     if (state.isValid === false) {
       return {
         value: action.value,
-        isValid: validator(action.value)
+        isValid: validator(action.value),
       };
     }
     return {
-      value: action.value
+      value: action.value,
     };
   }
-  if (action.type === 'INPUT_BLUR') {
+  if (action.type === "INPUT_BLUR") {
     return {
       value: action.value,
-      isValid: validator(action.value)
+      isValid: validator(action.value),
     };
   }
 
-  if (action.type === 'SUBMIT') {
+  if (action.type === "SUBMIT") {
     return {
-      isValid: validator(action.value)
+      isValid: validator(action.value),
     };
   }
 }
 
 function fieldDispath(dispath, ref, action) {
-  dispath({type: action, value: ref.current.value.trim()});
+  dispath({ type: action, value: ref.current.value.trim() });
 }
 
 const defaultFieldState = {
-  value: '',
-  isValid: null
+  value: "",
+  isValid: null,
 };
 
 function RegForm() {
@@ -55,52 +56,65 @@ function RegForm() {
   const passwordInputRef = useRef();
   const checkPassInputRef = useRef();
   const [passIsEqual, setPassIsEqual] = useState(true);
-  const [emailState, dispathEmail] =  useReducer((state, action) => {
-    return fieldReducer(state, action, validateEmail);
-  }, {...defaultFieldState});
-  const [passwordState, dispathPassword] =  useReducer((state, action) => {
-    return fieldReducer(state, action, validatePassword);
-  }, {...defaultFieldState});
-  const [checkPassState, dispathCheckPass] =  useReducer((state, action) => {
-    return fieldReducer(state, action, validatePassword);
-  }, {...defaultFieldState});
+  const [emailState, dispathEmail] = useReducer(
+    (state, action) => {
+      return fieldReducer(state, action, validateEmail);
+    },
+    { ...defaultFieldState }
+  );
+  const [passwordState, dispathPassword] = useReducer(
+    (state, action) => {
+      return fieldReducer(state, action, validatePassword);
+    },
+    { ...defaultFieldState }
+  );
+  const [checkPassState, dispathCheckPass] = useReducer(
+    (state, action) => {
+      return fieldReducer(state, action, validatePassword);
+    },
+    { ...defaultFieldState }
+  );
 
-  function emailChangeHandler() {
-    fieldDispath(dispathEmail, emailInputRef, 'USER_INPUT');
-  }
-  function emailOnBlur() {
-    fieldDispath(dispathEmail, emailInputRef, 'INPUT_BLUR');
-  }
-
-  function passwordChangeHandler() {
-    fieldDispath(dispathPassword, passwordInputRef, 'USER_INPUT');
-  }
-  function passwordOnBlur() {
-    fieldDispath(dispathPassword, passwordInputRef, 'INPUT_BLUR');
-  }
-
-  function checkPassChangeHandler() {
-    fieldDispath(dispathCheckPass, checkPassInputRef, 'USER_INPUT');
-    setPassIsEqual(true);
-  }
-  function checkPassOnBlur() {
-    fieldDispath(dispathCheckPass, checkPassInputRef, 'INPUT_BLUR');
-    if (passwordState.value === checkPassState.value) {
+  function inputChangeHandler(e) {
+    if (e.target === emailInputRef.current)
+      fieldDispath(dispathEmail, emailInputRef, "USER_INPUT");
+    if (e.target === passwordInputRef.current)
+      fieldDispath(dispathPassword, passwordInputRef, "USER_INPUT");
+    if (e.target === checkPassInputRef.current) {
+      fieldDispath(dispathCheckPass, checkPassInputRef, "USER_INPUT");
       setPassIsEqual(true);
-    } else {
-      setPassIsEqual(false);
+    }
+  }
+
+  function inputOnBlur(e) {
+    if (e.target === emailInputRef.current)
+      fieldDispath(dispathEmail, emailInputRef, "INPUT_BLUR");
+    if (e.target === passwordInputRef.current)
+      fieldDispath(dispathPassword, passwordInputRef, "INPUT_BLUR");
+    if (e.target === checkPassInputRef.current) {
+      fieldDispath(dispathCheckPass, checkPassInputRef, "INPUT_BLUR");
+      if (passwordState.value === checkPassState.value) {
+        setPassIsEqual(true);
+      } else {
+        setPassIsEqual(false);
+      }
     }
   }
 
   function regHandler(e) {
     e.preventDefault();
-    if (emailState.isValid && passwordState.isValid && checkPassState.isValid && passwordState.value === checkPassState.value) {
+    if (
+      emailState.isValid &&
+      passwordState.isValid &&
+      checkPassState.isValid &&
+      passwordState.value === checkPassState.value
+    ) {
       setPassIsEqual(true);
-      console.log('Reg is OK');
+      console.log("Reg is OK");
     } else {
-      fieldDispath(dispathEmail, emailInputRef, 'SUBMIT');
-      fieldDispath(dispathPassword, passwordInputRef, 'SUBMIT');
-      fieldDispath(dispathCheckPass, checkPassInputRef, 'SUBMIT');
+      fieldDispath(dispathEmail, emailInputRef, "SUBMIT");
+      fieldDispath(dispathPassword, passwordInputRef, "SUBMIT");
+      fieldDispath(dispathCheckPass, checkPassInputRef, "SUBMIT");
     }
   }
 
@@ -114,12 +128,12 @@ function RegForm() {
           input={{
             type: "text",
             name: "userName",
-            placeholder: 'Enter email'
+            placeholder: "Enter email",
           }}
-          onChange={emailChangeHandler}
+          onChange={inputChangeHandler}
           isValid={emailState.isValid}
-          errorMsg={'Invalid email'}
-          onBlur={emailOnBlur}
+          errorMsg={"Invalid email"}
+          onBlur={inputOnBlur}
           //value={emailState.value}
           customClasses={classes.row}
         />
@@ -130,12 +144,12 @@ function RegForm() {
           input={{
             type: "password",
             name: "newPassword",
-            placeholder: "******"
+            placeholder: "******",
           }}
-          onChange={passwordChangeHandler}
+          onChange={inputChangeHandler}
           isValid={passwordState.isValid}
-          errorMsg={'Password shoud be great then 5 symbols'}
-          onBlur={passwordOnBlur}
+          errorMsg={"Password shoud be great then 5 symbols"}
+          onBlur={inputOnBlur}
           customClasses={classes.row}
         />
         <Input
@@ -145,12 +159,16 @@ function RegForm() {
           input={{
             type: "password",
             name: "repeatPassword",
-            placeholder: "******"
+            placeholder: "******",
           }}
-          onChange={checkPassChangeHandler}
+          onChange={inputChangeHandler}
           isValid={passIsEqual && checkPassState.isValid}
-          errorMsg={passIsEqual ? 'Password shoud be great then 5 symbols' : 'The passwords should be match'}
-          onBlur={checkPassOnBlur}
+          errorMsg={
+            passIsEqual
+              ? "Password shoud be great then 5 symbols"
+              : "The passwords should be match"
+          }
+          onBlur={inputOnBlur}
           customClasses={classes.row}
         />
         <div className={`${classes["btn-row"]} ${classes.row}`}>
@@ -160,7 +178,7 @@ function RegForm() {
               type: "submit",
               id: "submitReg",
               name: "submitReg",
-              value: "1"
+              value: "1",
             }}
           />
         </div>
